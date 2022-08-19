cf0a9238e1258d22808205468c8dd8db15be25d4
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-19 14:47:08 +0530

    done

--- webPage/index.js
+++ webPage/index.js
@@ -3,6 +3,16 @@ const app = express();
 const path = require("path");
 const port = 8080;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+
+app.get('/:home', (req, res) => {
+    const {home} = req.params;
+    const randomNum = Math.floor(Math.random() * 100) +1
+    res.render('home', {url:home, randomNum})
+})
+
 
 app.listen(port, (err) => {
     if (err) {
